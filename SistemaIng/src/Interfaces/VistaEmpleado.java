660726ca352751ab3280e2048b24385fd4a7bed9
LautaroPalacin <lautaropalacin2015@gmail.com>
2017-06-04 13:08:01 -0300

    comiit

--- SistemaIng/src/Interfaces/VistaEmpleado.java
+++ SistemaIng/src/Interfaces/VistaEmpleado.java
@@ -59,7 +59,12 @@ public class VistaEmpleado extends javax.swing.JFrame {
 
         jButton4.setText("Info Plantel");
 
-        jButton5.setText("Modificar Plantel");
+        jButton5.setText("Modificar Vaca");
+        jButton5.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton5ActionPerformed(evt);
+            }
+        });
 
         jButton6.setText("Salir");
         jButton6.addActionListener(new java.awt.event.ActionListener() {
@@ -127,6 +132,11 @@ public class VistaEmpleado extends javax.swing.JFrame {
         this.setVisible(false);
         new LogIn().setVisible(true);
     }//GEN-LAST:event_VolverAInicio
+
+    private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
+        this.setVisible(false);
+        new ListadoDeVacas().setVisible(true);
+    }//GEN-LAST:event_jButton5ActionPerformed
 
     /**
      * @param args the command line arguments
